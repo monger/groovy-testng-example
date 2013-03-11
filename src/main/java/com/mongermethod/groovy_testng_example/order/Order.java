43e5df86ffa0b324acd7dd8db43eab7a013f0eac
monger <github@mongermethod.com>
2013-03-11 14:52:15 -0600

    Added equals() method

--- src/main/java/com/mongermethod/groovy_testng_example/order/Order.java
+++ src/main/java/com/mongermethod/groovy_testng_example/order/Order.java
@@ -1,5 +1,6 @@
 package com.mongermethod.groovy_testng_example.order;
 
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
@@ -12,10 +13,15 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderSequence")
     private long id;
+
     private String username;
+
+    @Column(name = "order_date")
+    @Temporal(TemporalType.DATE)
     private Date date;
-    @OneToMany(fetch = FetchType.EAGER)
-    @JoinColumn(name = "order_id")
+
+    @OneToMany(mappedBy = "orderId", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+
     private Set<Item> orderItems;
 
     public long getId() {
@@ -48,5 +54,37 @@ public class Order implements Serializable {
 
     public void setOrderItems(Set<Item> orderItems) {
         this.orderItems = orderItems;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (date.getTime() != order.date.getTime()) return false;
+        if (!username.equals(order.username)) return false;
+        if (orderItems != null) {
+            if (orderItems.size() != order.orderItems.size()) return false;
+            for (Item item : orderItems) {
+                boolean match = false;
+                for (Item oItem : order.getOrderItems()) {
+                    if (item.getId() == oItem.getId() &&
+                            item.getOrderId() == oItem.getOrderId() &&
+                            item.getSku().equals(oItem.getSku()) &&
+                            item.getPurchasePrice().equals(oItem.getPurchasePrice()) &&
+                            item.getQuantity() == oItem.getQuantity()) {
+                        match = true;
+                    }
+                }
+                if (!match) return false;
+            }
+        } else if(order.orderItems != null) {
+            return false;
+        }
+
+        return true;
     }
 }
