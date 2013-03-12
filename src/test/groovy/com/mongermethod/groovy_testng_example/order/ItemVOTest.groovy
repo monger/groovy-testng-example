4098fb800b2f2e8d10429ca96de84833bc556534
monger <github@mongermethod.com>
2013-03-12 08:46:20 -0600

    Completed Item value object unit test

--- src/test/groovy/com/mongermethod/groovy_testng_example/order/ItemVOTest.groovy
+++ src/test/groovy/com/mongermethod/groovy_testng_example/order/ItemVOTest.groovy
@@ -1,5 +1,29 @@
 package com.mongermethod.groovy_testng_example.order
+import com.mongermethod.groovy_testng_example.UnitBootstrap
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
 
+import javax.annotation.Resource
 
-class ItemVOTest {
+class ItemVOTest extends UnitBootstrap {
+    @Resource(name = "newOrderItem1")
+    Item testItem
+
+    @BeforeMethod
+    void setup() {
+        testItem.id = 5
+        testItem.orderId = 10
+    }
+
+    @Test(groups = "unit")
+    void "test the proper implementation of accessors and mutators"() {
+        def item = new Item(
+                id: testItem.id,
+                orderId: testItem.orderId,
+                sku: testItem.sku,
+                purchasePrice: testItem.purchasePrice,
+                quantity: testItem.quantity
+        )
+        assert item == testItem
+    }
 }
