837503f5dce915bea58a8d09627eb3dedb1df57e
monger <github@mongermethod.com>
2013-03-12 08:45:55 -0600

    Completed Order value object unit test

--- src/test/groovy/com/mongermethod/groovy_testng_example/order/OrderVOTest.groovy
+++ src/test/groovy/com/mongermethod/groovy_testng_example/order/OrderVOTest.groovy
@@ -1,5 +1,39 @@
 package com.mongermethod.groovy_testng_example.order
 
+import com.mongermethod.groovy_testng_example.UnitBootstrap
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
 
-class OrderVOTest {
+import javax.annotation.Resource
+
+
+class OrderVOTest extends UnitBootstrap {
+    @Resource(name =  "newOrder2")
+    Order testOrder
+    @Resource(name = "newOrderItemList2")
+    List<Item> testOrderItems
+
+    @BeforeMethod
+    void setup() {
+        def orderId = 25
+
+        testOrderItems.eachWithIndex { item, index ->
+            item.id = index + 1
+            item.orderId = orderId
+        }
+        testOrder.id = orderId
+        testOrder.orderItems = testOrderItems
+    }
+
+    @Test(groups = "unit")
+    void "test the proper implementation of accessors and mutators"() {
+        Order order = new Order(
+                id: testOrder.id,
+                username: testOrder.username,
+                date: testOrder.date,
+                orderItems: testOrderItems
+        )
+
+        assert order == testOrder
+    }
 }
